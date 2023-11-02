f5525547c018046b66c9dcb9ed15eb972ba9edda
LinhGithub <lamthatnhanh111@gmail.com>
2023-11-02 09:23:02 +0700

    add folder translation

---
+++ mbw_service_v2/translations/language.py
@@ -0,0 +1,82 @@
+translations = {
+    "successfully": {
+        "vi": "Thành công",
+        "en": "Success"
+    },
+    "error": {
+        "vi": "Có lỗi xảy ra.",
+        "en": "Error"
+    },
+    "faceid_success": {
+        "vi": "Cập nhật FaceID thành công.",
+        "en": "Updated FaceID successfully."
+    },
+    "not_face_recognition": {
+        "vi": "Không thể nhận dạng khuôn mặt.",
+        "en": "Face recognition is not possible."
+    },
+    "face_not_found": {
+        "vi": "Bạn chưa đăng ký khuôn mặt.",
+        "en": "You have not registered your face yet."
+    },
+    "faceid_register_success": {
+        "vi": "Đăng ký FaceID thành công.",
+        "en": "FaceID registration successful."
+    },
+    "faceid_verify_success": {
+        "vi": "Xác minh FaceID thành công.",
+        "en": "FaceID verification successful."
+    },
+    "faceid_verify_fail": {
+        "vi": "Ảnh chụp không đúng FaceID đã đăng ký.",
+        "en": "The photo was taken incorrectly with the registered FaceID."
+    },
+    "login_success": {
+        "vi": "Đăng nhập thành công",
+        "en": "Login successfully"
+    },
+    "login_error": {
+        "vi": "Đăng nhập thất bại vui lòng kiểm tra tài khoản hoặc mật khẩu",
+        "en": "Login fail! Please check user or password"
+    },
+    "logout_success": {
+        "vi": "Đăng xuất thành công",
+        "en": "Logout successfully"
+    },
+    "logout_error": {
+        "vi": "Đăng xuất thất bại",
+        "en": "Logout fail!"
+    },
+    "not_allow": {
+        "vi": "Không cho phép",
+        "en": "Not allowed!"
+    },
+    "email_send_success": {
+        "vi": "Mật khẩu email đã gửi",
+        "en": "Password email sent"
+    },
+    "email_send_fail": {
+        "vi": "Mật khẩu email gửi không thành công",
+        "en": "Password email sent fail"
+    },
+    "title_1": {
+        "vi": "Bạn phải",
+        "en": "You have to"
+    },
+    "check_in": {
+        "vi": "vào trước",
+        "en": "checkin first"
+    },
+    "check_out": {
+        "vi": "ra trước",
+        "en": "checkout first"
+    },
+    "create_success": {
+        "vi": "Tạo mới thành công",
+        "en": "Created successfully"
+    },
+    "create_fail": {
+        "vi": "Tạo mới thất bại",
+        "en": "Created fail"
+    }
+}
