e3f09bbca8a4caeb3583766bfa3f4e05ad1c0dc6
chuyendev <chuyendev@gmail.com>
2023-11-06 15:01:14 +0700

    cap nhat sap xep khoang cach

---
+++ mbw_service_v2/mbw_service_v2/report/mbw_monthly_attendance_sheet/mbw_monthly_attendance_sheet.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, chuyendev and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["MBW_Monthly Attendance Sheet"] = {
+	"filters": [
+
+	]
+};
---
+++ mbw_service_v2/mbw_service_v2/report/mbw_monthly_attendance_sheet_vi/mbw_monthly_attendance_sheet_vi.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, chuyendev and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["MBW Monthly Attendance Sheet vi"] = {
+	"filters": [
+
+	]
+};
