157ecb6a55dae9797b669a6ad5c5aae87a04b47f
chuyendev <chuyendev@gmail.com>
2023-11-09 16:25:26 +0700

    update chekin

--- mbw_service_v2/api/ess/checkin.py
+++ mbw_service_v2/api/ess/checkin.py
@@ -106,19 +106,6 @@ def get_list_cham_cong(**kwargs):
 @frappe.whitelist(methods="GET",allow_guest= True)
 def get_shift_now():
     try:
-        import socket
-        client_ip = frappe.local.request.remote_addr
-        import netifaces
-        import requests
-        import psutil
-
-        # Lấy địa chỉ IP mạng của giao diện mạng (ví dụ: eth0)
-        network_interfaces = psutil.net_if_addrs().keys() 
-        # return network_interfaces
-        network_interface = "enp2s0"
-        network_ip = netifaces.ifaddresses(network_interface)[netifaces.AF_INET][0]['addr']
-        print(f"Địa chỉ IP mạng của giao diện {network_interface}: {network_ip}")        
-        return network_ip
         name= get_employee_id()
         shift_now = get_shift_type_now(name)
         if shift_now["shift_type_now"]:
