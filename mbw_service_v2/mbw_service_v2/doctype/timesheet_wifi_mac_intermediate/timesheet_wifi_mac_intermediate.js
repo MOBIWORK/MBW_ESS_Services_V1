af61b5217e65268ba15f138f288f0ae26ce18cee
chuyendev <chuyendev@gmail.com>
2023-11-11 10:01:24 +0700

    change space wifi

--- mbw_service_v2/mbw_service_v2/doctype/timesheet_wifi_mac_intermediate/timesheet_wifi_mac_intermediate.js
+++ mbw_service_v2/mbw_service_v2/doctype/timesheet_wifi_mac_intermediate/timesheet_wifi_mac_intermediate.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, chuyendev and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('TimeSheet Wifi  mac intermediate', {
+frappe.ui.form.on('TimeSheet Wifi mac intermediate', {
 	// refresh: function(frm) {
 
 	// }
