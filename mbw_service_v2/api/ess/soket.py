3694ca82067c688f2d5940d10bfd42577e2b535a
chuyendev <chuyendev@gmail.com>
2023-11-22 15:47:59 +0700

    rs ot

--- mbw_service_v2/api/ess/soket.py
+++
@@ -1,7 +0,0 @@
-# In your Frappe backend code
-import frappe
-
-@frappe.whitelist()
-def handle_realtime_event(data):
-    # Process the data and broadcast the update
-    frappe.publish_realtime(event='some-event', message=data, user='admin', doctype='My DocType', docname='My Doc', after_commit=True, everyone=True, now=True, ondelete=True, room=frappe.get_request_site_address())
