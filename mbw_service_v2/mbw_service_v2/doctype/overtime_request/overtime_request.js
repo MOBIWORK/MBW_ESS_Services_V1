985ab552aea22c4ee71c4419469b53527eab4931
chuyendev <chuyendev@gmail.com>
2023-11-23 09:43:37 +0700

    update error message

--- mbw_service_v2/mbw_service_v2/doctype/overtime_request/overtime_request.js
+++ mbw_service_v2/mbw_service_v2/doctype/overtime_request/overtime_request.js
@@ -5,10 +5,11 @@ frappe.ui.form.on('Overtime Request', {
 	setup: function(frm) {
 		frm.set_query("ot_approver", function() {
 			return {
-				query: "hrms.hr.doctype.department_approver.department_approver.get_approvers",
+				query: "mbw_service_v2.mbw_service_v2.doctype.overtime_request.overtime_request.get_approvers",
 				filters: {
 					employee: frm.doc.employee,
-					doctype: frm.doc.doctype
+					doctype: frm.doc.doctype,
+					date: frm.doc.ot_date
 				}
 			};
 		});
@@ -25,6 +26,7 @@ frappe.ui.form.on('Overtime Request', {
 	},
 
 	employee: function(frm) {
+		frm.trigger("setup");
 		frm.trigger("set_ot_approver");
 	},
 
@@ -32,13 +34,16 @@ frappe.ui.form.on('Overtime Request', {
 	set_ot_approver: function(frm) {
 		if (frm.doc.employee) {
 			return frappe.call({
-				// method: "hrms.hr.doctype.leave_application.leave_application.get_leave_approver",
 				method: "mbw_service_v2.mbw_service_v2.doctype.overtime_request.overtime_request.get_ot_approver",
 				args: {
 					"employee": frm.doc.employee,
+					"doctype": frm.doc.doctype,
+					"date": frm.doc.ot_date
 				},
 				callback: function(r) {
-					if (r && r.message) {
+					console.log(r)
+					if (r) {
+						console.log("data",r.message)
 						frm.set_value("ot_approver", r.message);
 					}
 				}
