4457c21f1bf7de13e43cdc1c5a5f6d34a75a8ccd
chuyendev <chuyendev@gmail.com>
2023-12-01 09:09:49 +0700

    regex time

--- mbw_service_v2/api/ess/overtime.py
+++ mbw_service_v2/api/ess/overtime.py
@@ -10,8 +10,7 @@ from mbw_service_v2.api.common import  (get_last_check, gen_response,get_employe
     enable_check_shift,
     validate_datetime,
     validate_empty,
-    validate_image,
-    get_approver_detail
+    validate_image
     )
 
 from pypika import CustomFunction
@@ -30,8 +29,8 @@ def create_ot_request(**data):
         employee = get_employee_id()
         ot_date = datetime.fromtimestamp(int(data.get('ot_date'))).date()  if data.get('ot_date') else False
         shift = data.get('shift') if data.get('shift') else False
-        ot_start_time = datetime.strptime(data.get('ot_start_time'), "%H:%M").time()  if data.get('ot_start_time') else False
-        ot_end_time = datetime.strptime(data.get('ot_end_time'), "%H:%M").time() if data.get('ot_end_time') else False
+        ot_start_time = datetime.strptime(data.get('ot_start_time'), "%H:%M:%S").time()  if data.get('ot_start_time') else False
+        ot_end_time = datetime.strptime(data.get('ot_end_time'), "%H:%M:%S").time() if data.get('ot_end_time') else False
         ot_approver = data.get('ot_approver') if data.get('ot_approver') else False
         if not ( ot_date or shift or ot_start_time or ot_end_time or ot_approver ) :
             gen_response(500, i18n.t('translate.invalid_value', locale=get_language()), []) 
@@ -41,7 +40,7 @@ def create_ot_request(**data):
         data['ot_date'] = ot_date
         data['posting_date'] = datetime.now().date()
         data['suggested_time'] = (ot_end_time.hour*60 + ot_end_time.minute  - (ot_start_time.hour*60 + ot_start_time.minute))/60
-        field_in = ["ot_date", "shift", "ot_start_time", "ot_end_time", "ot_approver", "posting_date","employee","suggested_time"]
+        field_in = ["ot_date", "shift", "ot_start_time", "ot_end_time", "ot_approver", "posting_date","employee","suggested_time",'reason']
         for field, value in data.items() :
             if field not in field_in: 
                 gen_response(500, i18n.t('translate.invalid_value', locale=get_language()), []) 
@@ -49,8 +48,9 @@ def create_ot_request(**data):
             setattr(new_ot_rq, field, value)
         new_ot_rq.insert()
         gen_response(201, "", new_ot_rq)
-    except frappe.DoesNotExistError:
-        gen_response(404, i18n.t('translate.error', locale=get_language()), []) 
+    except Exception as e:
+        exception_handel(e)
+        # gen_response(404, i18n.t('translate.error', locale=get_language()), []) 
 
 # update ot request
 @frappe.whitelist(methods="PATCH")
@@ -61,8 +61,8 @@ def update_ot_request(**data):
         ot_rq = frappe.get_doc("Overtime Request",ot_name)
         ot_date = datetime.fromtimestamp(int(data.get('ot_date'))).date()  if data.get('ot_date') else False
         shift = data.get('shift') if data.get('shift') else False
-        ot_start_time = datetime.strptime(data.get('ot_start_time'), "%H:%M").time()  if data.get('ot_start_time') else False
-        ot_end_time = datetime.strptime(data.get('ot_end_time'), "%H:%M").time() if data.get('ot_end_time') else False
+        ot_start_time = datetime.strptime(data.get('ot_start_time'), "%H:%M:%S").time()  if data.get('ot_start_time') else False
+        ot_end_time = datetime.strptime(data.get('ot_end_time'), "%H:%M:%S").time() if data.get('ot_end_time') else False
         ot_approver = data.get('ot_approver') if data.get('ot_approver') else False
         if not ( ot_date or shift or ot_start_time or ot_end_time or ot_approver ) :
             gen_response(500, i18n.t('translate.invalid_value', locale=get_language()), []) 
@@ -75,8 +75,10 @@ def update_ot_request(**data):
             setattr(ot_rq, field, value)
         ot_rq.save()
         gen_response(201, "", ot_rq)
-    except frappe.DoesNotExistError:
-        gen_response(404, i18n.t('translate.error', locale=get_language()), []) 
+    except Exception as e:
+        frappe.clear_messages()
+        exception_handel(e)
+        # gen_response(404, i18n.t('translate.error', locale=get_language()), []) 
 
 
 # ot list
@@ -87,11 +89,14 @@ def get_list_ot_request(**params):
         employee_id = get_employee_id()
         employee_info = frappe.db.get_value("Employee",employee_id,['*'],as_dict=True)
         status = params.get("status") if params.get("status") else False
+        list_ot = frappe.db.get_list("Overtime Request",["name","ot_date", "shift", "ot_start_time", "ot_end_time", "ot_approver", "posting_date","employee","suggested_time","status"])
         OtRequest = frappe.qb.DocType("Overtime Request")
-        query = (OtRequest.employee == employee_id) 
+        field_in = ["ot_date", "shift", "ot_start_time", "ot_end_time", "ot_approver", "posting_date","employee","suggested_time"]
+        query = ((OtRequest.employee == employee_id) | (OtRequest.ot_approver == employee_info.get("user_id")))
         UNIX_TIMESTAMP = CustomFunction('UNIX_TIMESTAMP', ['day'])
         if status:
-            query = ((OtRequest.employee == employee_id)  & (status == OtRequest.status))
+            query = (((OtRequest.employee == employee_id) | (OtRequest.ot_approver == employee_info.get("user_id"))) & status == OtRequest.status)
+        print("query",query)
         list_ot_rq = (
             frappe.qb.from_(OtRequest)
             .where(query)
@@ -99,14 +104,44 @@ def get_list_ot_request(**params):
             .run(as_dict=True)
         )
 
-        queryDraft = frappe.db.count("Overtime Request",filters={"employee" : employee_id, "status":"Draft"})
-        queryApproved = frappe.db.count("Overtime Request",filters={"employee" : employee_id, "status":"Approved"})
-        queryRejected = frappe.db.count("Overtime Request",filters={"employee" : employee_id, "status":"Rejected"})
-        if len(list_ot_rq) > 0 :
-            for rq in list_ot_rq :
-                rq["approvers"] = get_approver_detail(employee_id,"ot_approver")
+        approver = validate_link(doctype='Employee',docname= employee_id,fields=json.dumps(["employee_name","department","ot_approver"]))
+        Employee = frappe.qb.DocType("Employee")
+        User = frappe.qb.DocType("User")
+        approver_info = (frappe.qb.from_(User)
+                         .inner_join(Employee)
+                         .on(User.email == Employee.user_id)
+                         .where(User.email ==  approver['ot_approver'])
+                         .select(Employee.employee_name.as_("full_name"),Employee.image,User.email)
+                         .run(as_dict=True)
+                         )
+        
+        if len(approver_info) > 0: 
+            approver_info = approver_info[0]
+            approver_info["image"] = validate_image(approver_info.get("image"))
+        else: 
+            approver_info = None
+
+        print("employee",employee_id)
+        print("approver",employee_info.get("user_id"))
+        queryDraft = len(frappe.qb.from_(OtRequest)
+            .where(((OtRequest.employee == employee_id) | (OtRequest.ot_approver == employee_info.get("user_id"))) & ("Draft" == OtRequest.status))
+            .select(OtRequest.employee,OtRequest.ot_date,OtRequest.shift,OtRequest.ot_date,OtRequest.ot_start_time,OtRequest.ot_end_time,OtRequest.ot_approver,OtRequest.posting_date,OtRequest.suggested_time,OtRequest.status)
+            .run(as_dict=True) )
+        queryApproved = len( frappe.qb.from_(OtRequest)
+            .where(((OtRequest.employee == employee_id) | (OtRequest.ot_approver == employee_info.get("user_id"))) & ("Approved" == OtRequest.status))
+            .select(OtRequest.employee,OtRequest.ot_date,OtRequest.shift,OtRequest.ot_date,OtRequest.ot_start_time,OtRequest.ot_end_time,OtRequest.ot_approver,OtRequest.posting_date,OtRequest.suggested_time,OtRequest.status)
+            .run(as_dict=True)
+                         )
+        queryRejected = len( frappe.qb.from_(OtRequest)
+            .where(((OtRequest.employee == employee_id) | (OtRequest.ot_approver == employee_info.get("user_id"))) & ("Rejected" == OtRequest.status))
+            .select(OtRequest.employee,OtRequest.ot_date,OtRequest.shift,OtRequest.ot_date,OtRequest.ot_start_time,OtRequest.ot_end_time,OtRequest.ot_approver,OtRequest.posting_date,OtRequest.suggested_time,OtRequest.status)
+            .run(as_dict=True)
+                         )
+        status = params.get("status") if params.get("status") else False
+
         gen_response(200,"",{
             "data": list_ot_rq,
+            "user_approver": approver_info,
             "queryDraft": queryDraft,
             "queryApproved": queryApproved,
             "queryRejected": queryRejected,
