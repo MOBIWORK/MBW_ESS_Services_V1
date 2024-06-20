37a868c0cfbcc86a7c9fe3fe88ffa9c0cde2b5ea
chuyendev <chuyendev@gmail.com>
2024-06-20 11:03:45 +0700

    zoom map

--- mbw_service_v2/public/js/map_default.js
+++ mbw_service_v2/public/js/map_default.js
@@ -7,7 +7,7 @@ const map_settings = frappe.provide("frappe.utils.map_defaults");
 // // New default location (middle of germany).
 map_settings.center = [105.777775,21.055938];
 // // new zoomlevel: see the whole country, not just a single city
-
+map_settings.zoom = 20
 // // Use a different map: satellite instead of streets
 map_settings.tiles = "https://api.ekgis.vn/v2/maps/osmplus/{x}/{y}/{z}.pbf?api_key=tpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
 map_settings.options.attribution = "Tiles &copy; Esri &mdash; Source: Ekgis Community";
