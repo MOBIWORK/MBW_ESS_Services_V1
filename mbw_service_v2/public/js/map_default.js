fca86313f6f9e051cdfd50c63e011da220175533
chuyendev <chuyendev@gmail.com>
2024-06-20 10:46:18 +0700

    map

--- mbw_service_v2/public/js/map_default.js
+++ mbw_service_v2/public/js/map_default.js
@@ -10,5 +10,7 @@ map_settings.center = [105.777775,21.055938];
 map_settings.zoom = 20;
 
 // // Use a different map: satellite instead of streets
-map_settings.tiles = "https://api.ekgis.vn/v2/maps/raster/osm/bright/10/25/14.png?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
+map_settings.tiles = "https://api.ekgis.vn/v2/maps/raster/osm/bright/5/25/14.png?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
 map_settings.options.attribution = "Tiles &copy; Esri &mdash; Source: Ekgis Community";
+
+console.log
