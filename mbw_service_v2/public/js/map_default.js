00eba97c0d606fce50430f4ae8f264a56f463dc3
chuyendev <chuyendev@gmail.com>
2024-06-20 10:40:25 +0700

    fix map

--- mbw_service_v2/public/js/map_default.js
+++ mbw_service_v2/public/js/map_default.js
@@ -10,5 +10,5 @@ map_settings.center = [105.777775,21.055938];
 map_settings.zoom = 20;
 
 // // Use a different map: satellite instead of streets
-map_settings.tiles = "https://api.ekgis.vn/v2/maps/raster/osm/bright/{z}/{x}/{y}.png?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
+map_settings.tiles = "https://api.ekgis.vn/v2/maps/raster/osm/bright/10/25/14.png?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
 map_settings.options.attribution = "Tiles &copy; Esri &mdash; Source: Ekgis Community";
