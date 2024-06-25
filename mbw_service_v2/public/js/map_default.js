3bba5c2f62d666c85014de1801eed28d5c501a6e
chuyendev <chuyendev@gmail.com>
2024-06-25 08:57:48 +0700

    fix map

--- mbw_service_v2/public/js/map_default.js
+++ mbw_service_v2/public/js/map_default.js
@@ -2,5 +2,6 @@
 const map_settings = frappe.provide("frappe.utils.map_defaults");
 // // Use a different map: satellite instead of streets
 map_settings.center =  [105.777547,21.056248]
-map_settings.tiles = "https://api.ekgis.vn/v2/maps/osmplus/{x}/{y}/{z}.pbf?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
+map_settings.tiles = "https://api.ekgis.vn/v2/maps/osmplus/{z}/{x}/{y}.pbf?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
 map_settings.options.attribution = "Tiles &copy; Esri &mdash; Source: Ekgis Community";
+console.log("bbbbbbbb", "https://api.ekgis.vn/v2/maps/osmplus/{z}/{x}/{y}.pbf" );
