fb86b3eff99119a5e0282316efef917b2cc033b5
chuyendev <chuyendev@gmail.com>
2024-06-25 09:06:40 +0700

    change type map

--- mbw_service_v2/public/js/map_default.js
+++ mbw_service_v2/public/js/map_default.js
@@ -2,6 +2,6 @@
 const map_settings = frappe.provide("frappe.utils.map_defaults");
 // // Use a different map: satellite instead of streets
 map_settings.center =  [105.777547,21.056248]
-map_settings.tiles = "https://api.ekgis.vn/v2/maps/osmplus/{z}/{x}/{y}.pbf?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
-map_settings.options.attribution = "Tiles &copy; Esri &mdash; Source: Ekgis Community";
+map_settings.tiles = "https://api.ekgis.vn/v2/maps/raster/ctm/bright/{z}/{x}/{y}.png?api_key=wtpM0U1ZmE2s87LEZNSHf63Osc1a2sboaozCQNsy" ;
+map_settings.options.attribution = "Vector &copy; eKGis &mdash; Source: eKMap Service";
 console.log("bbbbbbbb", "https://api.ekgis.vn/v2/maps/osmplus/{z}/{x}/{y}.pbf" );
